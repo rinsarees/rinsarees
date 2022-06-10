a2ddcebdec95ce6b75b46d9a80977e1aeb7db59e
rinsarees <97509295+rinsarees@users.noreply.github.com>
2022-06-10 16:35:23 +0530

    Add files via upload

---
+++ complaints.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS complaints( 
+	id INT NOT NULL AUTO_INCREMENT, 
+	user_id INT, 
+  	complaint text NOT NULL,
+  	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(id), 
+	FOREIGN KEY(user_id) REFERENCES users(id)
+);
+
+CREATE TABLE SOLUTION( )
