f2ed6bf879be51f123d4129be9caf4421195d3b0
rinsarees <97509295+rinsarees@users.noreply.github.com>
2022-06-10 17:49:04 +0530

    Update complaints.sql

--- complaints.sql
+++ complaints.sql
@@ -1,4 +1,21 @@
+
 CREATE TABLE IF NOT EXISTS users( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS agents( 
+	id int NOT NULL AUTO_INCREMENT, 
+	name VARCHAR(100) NOT NULL, 
+	email VARCHAR(100) NOT NULL, 
+  	password varchar(50) NOT NULL,
+	PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS employees( 
 	id int NOT NULL AUTO_INCREMENT, 
 	name VARCHAR(100) NOT NULL, 
 	email VARCHAR(100) NOT NULL, 
@@ -9,10 +26,34 @@ CREATE TABLE IF NOT EXISTS users(
 CREATE TABLE IF NOT EXISTS complaints( 
 	id INT NOT NULL AUTO_INCREMENT, 
 	user_id INT, 
+	agent_id INT, 
+	employee_id INT, 
   	complaint text NOT NULL,
+  	status VARCHAR(20) NOT NULL,
   	`date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(id), 
-	FOREIGN KEY(user_id) REFERENCES users(id)
+	FOREIGN KEY(user_id) REFERENCES users(id),
+	FOREIGN KEY(agent_id) REFERENCES agents(id),
+	FOREIGN KEY(employee_id) REFERENCES employees(id)
 );
 
-CREATE TABLE SOLUTION( )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
